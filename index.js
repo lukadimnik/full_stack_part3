1a028f287edfcc162891f246b9d48820f13c7965
Luka Dimnik <luka.dimnik88@gmail.com>
2020-05-01 21:29:11 +0300

    minor changes

--- index.js
+++ index.js
@@ -113,10 +113,10 @@ app.delete("/api/persons/:id", (request, response) => {
 app.put("/api/persons/:id", (request, response, next) => {
   const body = request.body;
   console.log("body: ", body);
-  const person = new Person({
+  const person = {
     name: body.name,
     number: body.number,
-  });
+  };
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then((updatedPerson) => {
